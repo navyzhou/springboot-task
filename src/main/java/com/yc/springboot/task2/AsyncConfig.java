382a273cd8182717b4dc41bc37eb68833f58a5e8
navyzhou <qfxsxhfy@126.com>
2020-05-04 21:35:42 +0800

    多线程异步执行

--- src/main/java/com/yc/springboot/task2/AsyncConfig.java
+++ src/main/java/com/yc/springboot/task2/AsyncConfig.java
@@ -17,17 +17,19 @@ import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 @Configuration // 表明该类是一个配置类
 @EnableAsync // 开启异步事件的支持
 public class AsyncConfig {
-	// 此处成员变量应该使用@Value从配置中读取
-	private int corePoolSize = 10;
-	private int maxPoolSize = 200;
-	private int queueCapacity = 10;
-	
+	// 此处成员变量可以使用@Value()从配置文件中读取
+	private int corePoolSize = 10;  // 核心线程数
+	private int maxPoolSize = 200; // 最大线程数
+	private int queueCapacity = 10; // 队列大小
+	private int keepAliveSeconds = 3000; // 最大空闲时间
+
 	@Bean
 	public Executor taskExecutor() {
 		ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
 		executor.setCorePoolSize(corePoolSize);
 		executor.setMaxPoolSize(maxPoolSize);
 		executor.setQueueCapacity(queueCapacity);
+		executor.setKeepAliveSeconds(keepAliveSeconds);
 		executor.initialize();
 		return executor;
 	}
